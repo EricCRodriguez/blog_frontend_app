4a7e6aa92e221819fd3d9f900d53d7e6d4549a66
EricCRodriguez <ecrod12@gmail.com>
2020-11-14 17:32:47 -0600

    added bootstrap elements to public folder etc

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    !-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <button
+          class="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="post.html">Sample Post</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,94 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>New Post</h1>
+    <div>
+      Title
+      <input type="text" v-model="newPostTitle" />
+      Body:
+      <input type="text" v-model="newPostBody" />
+      Image:
+      <input type="text" v-model="newPostImage" />
+      <button v-on:click="createPost()">Create Post</button>
+    </div>
+
+    <h1>All Post</h1>
+    <div v-for="post in posts">
+      <h2>{{ post.title }}</h2>
+      <img v-bind:src="post.url" v-bind:alt="post.title" />
+      <button v-on:click="showPost(post)">Show more</button>
+      <div v-if="currentPost === post">
+        <p>Body: {{ post.body }}</p>
+        <p>Image: {{ post.image }}</p>
+      </div>
+      Title:
+      <input type="text" v-model="post.title" />
+      Body:
+      <input type="text" v-model="post.body" />
+      Image:
+      <input type="text" v-model="post.image" />
+      <button v-on:click="updatePost(post)">Update Post</button>
+      <button v-on:click="destroyPost(post)">Destroy Post</button>
+    </div>
   </div>
 </template>
+
 <style></style>
+
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      posts: [],
+      currentPost: {},
+      newPostTitle: "",
+      newPostBody: "",
+      newPostImage: ""
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    axios.get("/api/posts").then((response) => {
+      this.posts = response.data;
+    });
+  },
+  methods: {
+    createPost: function() {
+      var params = {
+        title: this.newPostTitle,
+        body: this.newPostBody,
+        image: this.newPostImage
+      };
+      axios.post("/api/posts", params).then((response) => {
+        this.posts.push(response.data);
+        this.newPostTitle = "";
+        this.newPostBody = "";
+        this.newPostImage = "";
+      });
+    },
+    showPost: function(post) {
+      if (this.currentPost === post) {
+        this.currentPost = {};
+      } else {
+        this.currentPost = post;
+      }
+    },
+    updatePost: function(post) {
+      var params = {
+        title: post.title,
+        body: post.body,
+        image: post.image
+      };
+      axios.patch("/api/posts/" + post.id, params).then((response) => {
+        this.currentPost = {};
+      });
+    },
+    destroyPost: function(post) {
+      axios.delete("/api/posts/" + post.id).then((response) => {
+        var index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      });
+    }
+  }
 };
 </script>
