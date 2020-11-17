447cc00c6e72555ced44f40ae7841774b1d2ec75
EricCRodriguez <ecrod12@gmail.com>
2020-11-16 19:08:28 -0600

    added images/setup New Post Function on home page

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <a class="navbar-brand" href="index.html"></a>
         <button
           class="navbar-toggler navbar-toggler-right"
           type="button"
@@ -34,19 +34,32 @@
     </nav>
 
     <!-- Page Header -->
-    <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <header class="masthead" style="background-image: url('img/einstein.jpg')">
       <div class="overlay"></div>
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="site-heading">
-              <h1>Clean Blog</h1>
-              <span class="subheading">A Blog Theme by Start Bootstrap</span>
+              <h1>TIL Blog</h1>
+              <span class="subheading">by Eric Rodriguez</span>
             </div>
           </div>
         </div>
       </div>
     </header>
+
+    <!-- Body -->
+    <h1>New Post</h1>
+    <div>
+      Title
+      <input type="text" v-model="newPostTitle" />
+      Body:
+      <input type="text" v-model="newPostBody" />
+      Image:
+      <input type="text" v-model="newPostImage" />
+      <button v-on:click="createPost()">Create Post</button>
+    </div>
+
     <!-- Main Content -->
     <div class="container">
       <div class="row">
@@ -62,48 +75,13 @@
             </a>
             <p class="post-meta">
               Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 24, 2019
+              <a href="#">Eric Rodriguez</a>
             </p>
           </div>
 
           <hr />
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                Science has not yet mastered prophecy
-              </h2>
-              <h3 class="post-subtitle">
-                We predict too much for the next year and yet far too little for the next ten.
-              </h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#">Start Bootstrap</a>
-              on August 24, 2019
-            </p>
-          </div>
-          <hr />
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                Failure is not an option
-              </h2>
-              <h3 class="post-subtitle">
-                Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-              </h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#">Start Bootstrap</a>
-              on July 8, 2019
-            </p>
-          </div>
-          <hr />
+
           <!-- Pager -->
-          <div class="clearfix">
-            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-          </div>
         </div>
       </div>
     </div>
@@ -115,3 +93,61 @@
 </template>
 
 <style></style>
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      posts: [],
+      currentPost: {},
+      newPostTitle: "",
+      newPostBody: "",
+      newPostImage: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/posts").then((response) => {
+      this.posts = response.data;
+    });
+  },
+  methods: {
+    createPost: function() {
+      var params = {
+        title: this.newPostTitle,
+        body: this.newPostBody,
+        image: this.newPostImage
+      };
+      axios.post("/api/posts", params).then((response) => {
+        this.posts.push(response.data);
+        this.newPostTitle = "";
+        this.newPostBody = "";
+        this.newPostImage = "";
+      });
+    },
+    showPost: function(post) {
+      if (this.currentPost === post) {
+        this.currentPost = {};
+      } else {
+        this.currentPost = post;
+      }
+    },
+    updatePost: function(post) {
+      var params = {
+        title: post.title,
+        body: post.body,
+        image: post.image
+      };
+      axios.patch("/api/posts/" + post.id, params).then((response) => {
+        this.currentPost = {};
+      });
+    },
+    destroyPost: function(post) {
+      axios.delete("/api/posts/" + post.id).then((response) => {
+        var index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      });
+    }
+  }
+};
+</script>
